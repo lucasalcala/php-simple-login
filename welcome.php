b24038f0075427e6af9a5b071ad9eca675b03914
Lucas Alcala <lucas_lap8@hotmail.com>
2018-05-24 11:29:19 -0400

    update
    comments

--- welcome.php
+++ welcome.php
@@ -1,9 +1,14 @@
 <?php
 
+//variables that will store username and password
+
 $uname = "admin";
 $pwd = "admin";
 
+// start session method
 session_start();
+
+//if statement that catches user info and displays the welcome page
 
 if(isset($_SESSION['uname'])){
 	
@@ -14,6 +19,7 @@ if(isset($_SESSION['uname'])){
 	echo "<br><a href='logout.php'><input type=button value=logout name=logout></a>";
 }
 else{
+//verifies that username and password matches and sends them to the welcome page if login matches
 	if($_POST['uname']==$uname && $_POST['pwd']==$pwd){
 		
 		$_SESSION['uname']==$uname;
@@ -21,7 +27,7 @@ else{
 		echo "<script>location.href='welcome.php'</script>";
 	}
 	else{
-		
+//if username or password are incorrect, it will send an alert using javascript and refresh the page until username and password matches. 		
 		echo "<script>alert('username or password incorrect!')</script>";
 		
 		echo"<script>location.href='index.php'</script>";
